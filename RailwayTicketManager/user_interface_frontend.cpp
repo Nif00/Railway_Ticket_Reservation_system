ba012c03c74c56c86a2eb43de11c44d423dc76b6
emirselimnergiz <emirselimnergiz@gmail.com>
2023-05-01 22:41:10 +0300

    final

--- RailwayTicketManager/user_interface_frontend.cpp
+++ RailwayTicketManager/user_interface_frontend.cpp
@@ -14,33 +14,38 @@ void gotoxy(int x, int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
+
 int main()
 {
-	int Set[] = { 7,7,7 };   //Default colors
-	int counter = 2;
+	int Set[] = { 7,7,7,7 };   //Default colors
+	int counter = 3;
 	char key;
 
-	for(int i = 0;;)
+	for (int i = 0;;)
 	{
 		gotoxy(10, 5);
 		color(Set[0]);
-		cout << "1. Menu";
+		cout << "1. Campaign";
 
 		gotoxy(10, 6);
 		color(Set[1]);
-		cout << "2. Settings";
+		cout << "2. Train";
 
 		gotoxy(10, 7);
 		color(Set[2]);
-		cout << "3. Help";
+		cout << "3. Ticket";
+
+		gotoxy(10, 8);
+		color(Set[3]);
+		cout << "4. Passenger";
 
 		key = _getch();
 
-		if (key == 72 && (counter >= 2 && counter <= 3))   //72 = up arrow key
+		if (key == 72 && (counter >= 2 && counter <= 4))   //72 = up arrow key
 		{
 			counter--;
 		}
-		if (key == 80 && (counter >= 1 && counter <= 2))   //80 = down arrow key
+		if (key == 80 && (counter >= 1 && counter <= 3))   //80 = down arrow key
 		{
 			counter++;
 		}
@@ -48,24 +53,29 @@ int main()
 		{
 			if (counter == 1)
 			{
-				cout << "Menu is open";
+				cout << " Campaign selection is open";
 			}
 			if (counter == 2)
 			{
-				cout << "Settings is open";
+				cout << " Train selection is open";
 			}
 			if (counter == 3)
 			{
-				cout << "Help is open";
+				cout << " Ticket selection is open";
+			}
+			if (counter == 4)
+			{
+				cout << " Passenger selection is open";
 			}
 		}
 		Set[0] = 7;
 		Set[1] = 7;
 		Set[2] = 7;
+		Set[3] = 7;
 
 		if (counter == 1)
 		{
-			Set[0] = 12;   
+			Set[0] = 12;
 		}
 		if (counter == 2)
 		{
@@ -73,7 +83,11 @@ int main()
 		}
 		if (counter == 3)
 		{
-			Set[2] = 12;   
+			Set[2] = 12;
+		}
+		if (counter == 4)
+		{
+			Set[3] = 12;
 		}
 	}
 	return 0;
